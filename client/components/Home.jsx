20b7a4abf1a46c8c59489c4437e623b9b3f1eea4
Alexander McKerrow <mckerrow4@gmail.com>
2021-04-22 13:44:02 +1200

    home link to regiter added, validate and requiring form elements

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-function Home (){
-return(
-    <>
-
-    </>
-)
+function Home() {
+    return (
+        <>
+            <div className="container is-fluid">
+                <div>
+                    <img src='../../images/logo.png' />
+                </div>
+                <div>
+                    <h1>Welcome to the Skin Specialist Centre</h1>
+                    <Link to={'/Register'}>
+                    <img src="../../images/footer-logo.png" />
+                    <p>Please click here to fill in your registration form</p>
+                    </Link>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default Home
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useForm } from "react-hook-form"
 
 function Register() {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
     const onSubmit = data => console.log(data)
 
     return (
@@ -11,84 +11,127 @@ function Register() {
             <form onSubmit={handleSubmit(onSubmit)}>
                 {/* personal details */}
                 <div>
-
                     <div className="field">
-                        <label >NHI:</label>
+                        <label >National Health Index:</label>
                         <br />
-                        <input {...register("nhi")} type="text" placeholder="NHI" />
+                        <input {...register("nHI")} type="text" placeholder="#" />
                     </div>
                     <div className="field">
                         <label>Title</label>
                         <br />
-                        <select {...register("title")}>
-                            <option>Please select one</option>
+                        <select {...register("title", { required: true })}>
+                            <option></option>
                             <option>Mr</option>
                             <option>Miss</option>
                             <option>Mrs</option>
                             <option>Ms</option>
                         </select>
+                        <span className="has-text-danger">
+                            {errors.title && "Title is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label >First Name:</label>
                         <br />
-                        <input {...register("firstName")} type="text" placeholder="First name" />
+                        <input {...register("firstName", { required: true })} type="text" placeholder="Alexander" />
+                        <span className="has-text-danger">
+                            {errors.firstName && "First name is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label>Surname:</label>
                         <br />
-                        <input {...register("surname")} type="text" placeholder="Surname" />
+                        <input {...register("surname", { required: true })} type="text" placeholder="Smith" />
+                        <span className="has-text-danger">
+                            {errors.surname && "Surname is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label>Preferred Name:</label>
                         <br />
-                        <input {...register("preferredName")} type="text" placeholder="Preferred Name" />
+                        <input {...register("preferredName")} type="text" placeholder="Alex" />
                     </div>
                     <div className="field">
-                        <label>Phone:</label>
+                        <label>Date of Birth:</label>
                         <br />
-                        <input {...register("phone")} type="text" placeholder="Phone" />
+                        <label>Day:</label>
+                        <input {...register("dobDay", { required: true })} maxLength="2" type="text" placeholder="DD" />
+                        <span className="has-text-danger">
+                            {errors.dobDay && "Day is required"}
+                        </span>
+                        <label>Month:</label>
+                        <input {...register("dobMonth", { required: true })} maxLength="2" type="text" placeholder="MM" />
+                        <span className="has-text-danger">
+                            {errors.dobMonth && "Month is required"}
+                        </span>
+                        <label>Year:</label>
+                        <input {...register("dobYear", { required: true })} maxLength="4" type="text" placeholder="YYYY" />
+                        <span className="has-text-danger">
+                            {errors.dobYear && "Year is required"}
+                        </span>
+
+                    </div>
+                    <div className="field">
+                        <label>Phone Number:</label>
+                        <br />
+                        <input {...register("phoneNumber", { required: true })} type="text" />
+                        <span className="has-text-danger">
+                            {errors.phoneNumber && "Phone number is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label>Email:</label>
                         <br />
-                        <input {...register("email")} type="text" placeholder="Email" />
+                        <input {...register("email")} type="text" placeholder="alex@smith.com" />
                     </div>
 
                     {/* GP Details */}
                     <div className="field">
                         <label>GP Name:</label>
                         <br />
-                        <input {...register("gpName")} type="text" placeholder="GP Name" />
+                        <input {...register("gpName", { required: true })} type="text" />
+                        <span className="has-text-danger">
+                            {errors.gpName && "GP name is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label>GP Suburb:</label>
                         <br />
-                        <input {...register("gpSuburb")} type="text" placeholder="GP Suburb" />
+                        <input {...register("gpSuburb", { required: true })} type="text" />
+                        <span className="has-text-danger">
+                            {errors.gpSuburb && "GP Suburb is required"}
+                        </span>
                     </div>
 
                     {/* medication & allergies */}
                     <div className="field">
-                        <label>Current Medications(Including supplements):</label>
+                        <label>Current Medications (Including supplements):</label>
                         <br />
-                        <input {...register("currentMed")} type="text" placeholder="Current Medication" />
+                        <input {...register("currentMed", { required: true })} type="text" placeholder="If no current medication say none" />
+                        <span className="has-text-danger">
+                            {errors.currentMed && "Current Medications is required"}
+                        </span>
                     </div>
                     <div className="field">
                         <label>Drug Allergies(Name of medication and description of reaction):</label>
                         <br />
-                        <input {...register("drugAllergies")} type="text" placeholder="Drug Allergies" />
+                        <input {...register("drugAllergies", { required: true })} type="text" placeholder="If no known drug allergies please say none" />
+                        <span className="has-text-danger">
+                            {errors.drugAllergies && "Drug allergies is required"}
+                        </span>
                     </div>
 
                     {/* Insurance */}
                     <div className="field">
                         <label>Name of Insurance Company:</label>
                         <br />
-                        <input {...register("insuranceCompany")} type="text" placeholder="Insurance Company" />
+                        <input {...register("insuranceCompany")} type="text" placeholder="eg: Southern Cross" />
                     </div>
 
                     <div className="field">
                         <label>Membership Number:</label>
                         <br />
-                        <input {...register("membershipNumber")} type="text" placeholder="Insurance Company" />
+                        <input {...register("membershipNumber")} type="text" placeholder="#" />
                     </div>
 
                     {/* Resident */}
@@ -97,11 +140,12 @@ function Register() {
                         <label>Are you an New Zealand Resident?</label>
                         <br />
                         <label className="radio">
-                            <input {...register("resident")} value="Yes" type="radio" />
+                            <input {...register("resident", { required: true })} value="Yes" type="radio" />
+                            
                     Yes
                     </label>
                         <label className="radio">
-                            <input {...register("resident")} value="No" type="radio" />
+                            <input {...register("resident", { required: true })} value="No" type="radio" />
                         No
                     </label>
 
@@ -117,8 +161,11 @@ function Register() {
                         </span>
                         <br />
                         <label>
-                            <input {...register("agreeToTerms")} value="accepted" type="checkbox" />
+                            <input {...register("agreeToTerms", { required: true })} value="accepted" type="checkbox" />
                             <strong> I have read and understood the above statement</strong>
+                            <span className="has-text-danger">
+                                {errors.agreeToTerms && "You must read the above statement and confirm the checkbox"}
+                            </span>
                         </label>
                     </div>
                     <div className="field">
